2f325c93999d82d57ed2c344df1c578d4cd32800
mcollomb <mcollomb@articque.com>
2022-03-25 16:15:05 +0100

    wip

---
+++ kelio-client/MainForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace kelio_client
+{
+  partial class MainForm
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+      this.button1 = new System.Windows.Forms.Button();
+      this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label6 = new System.Windows.Forms.Label();
+      this.panel1 = new System.Windows.Forms.Panel();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.panel1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+      this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.ForeColor = System.Drawing.Color.White;
+      this.button1.Location = new System.Drawing.Point(12, 40);
+      this.button1.Margin = new System.Windows.Forms.Padding(2);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(172, 40);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Badger";
+      this.button1.UseVisualStyleBackColor = false;
+      // 
+      // linkLabel1
+      // 
+      this.linkLabel1.ActiveLinkColor = System.Drawing.Color.GhostWhite;
+      this.linkLabel1.AutoSize = true;
+      this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.linkLabel1.LinkColor = System.Drawing.Color.CornflowerBlue;
+      this.linkLabel1.Location = new System.Drawing.Point(12, 12);
+      this.linkLabel1.Margin = new System.Windows.Forms.Padding(0);
+      this.linkLabel1.Name = "linkLabel1";
+      this.linkLabel1.Size = new System.Drawing.Size(125, 20);
+      this.linkLabel1.TabIndex = 1;
+      this.linkLabel1.TabStop = true;
+      this.linkLabel1.Text = "***REMOVED***";
+      this.linkLabel1.UseCompatibleTextRendering = true;
+      this.linkLabel1.VisitedLinkColor = System.Drawing.Color.CornflowerBlue;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point(168, 12);
+      this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+      this.pictureBox1.TabIndex = 2;
+      this.pictureBox1.TabStop = false;
+      this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.ForeColor = System.Drawing.Color.Orange;
+      this.label2.Location = new System.Drawing.Point(34, 0);
+      this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(34, 13);
+      this.label2.TabIndex = 2;
+      this.label2.Text = "12:15";
+      this.label2.Click += new System.EventHandler(this.label2_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.ForeColor = System.Drawing.Color.MediumSeaGreen;
+      this.label1.Location = new System.Drawing.Point(0, 0);
+      this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(34, 13);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "08:30";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.ForeColor = System.Drawing.Color.Silver;
+      this.label3.Location = new System.Drawing.Point(136, 98);
+      this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(48, 13);
+      this.label3.TabIndex = 5;
+      this.label3.Text = "Semaine";
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.ForeColor = System.Drawing.Color.Silver;
+      this.label4.Location = new System.Drawing.Point(153, 143);
+      this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(31, 13);
+      this.label4.TabIndex = 6;
+      this.label4.Text = "Total";
+      // 
+      // label5
+      // 
+      this.label5.ForeColor = System.Drawing.Color.MediumSeaGreen;
+      this.label5.Location = new System.Drawing.Point(146, 111);
+      this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(38, 14);
+      this.label5.TabIndex = 7;
+      this.label5.Text = "+2:30";
+      this.label5.TextAlign = System.Drawing.ContentAlignment.TopRight;
+      // 
+      // label6
+      // 
+      this.label6.ForeColor = System.Drawing.Color.Orange;
+      this.label6.Location = new System.Drawing.Point(146, 156);
+      this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(38, 14);
+      this.label6.TabIndex = 8;
+      this.label6.Text = "- 1:00";
+      this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+      // 
+      // panel1
+      // 
+      this.panel1.AutoScroll = true;
+      this.panel1.Controls.Add(this.label2);
+      this.panel1.Controls.Add(this.label1);
+      this.panel1.Location = new System.Drawing.Point(12, 98);
+      this.panel1.Margin = new System.Windows.Forms.Padding(2);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(75, 81);
+      this.panel1.TabIndex = 4;
+      this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+      // 
+      // MainForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.Black;
+      this.ClientSize = new System.Drawing.Size(192, 189);
+      this.Controls.Add(this.label6);
+      this.Controls.Add(this.label5);
+      this.Controls.Add(this.label4);
+      this.Controls.Add(this.label3);
+      this.Controls.Add(this.panel1);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.linkLabel1);
+      this.Controls.Add(this.button1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Margin = new System.Windows.Forms.Padding(2);
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "MainForm";
+      this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+      this.Text = "Kelio";
+      this.Load += new System.EventHandler(this.MainForm_Load);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.LinkLabel linkLabel1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.Label label6;
+    private System.Windows.Forms.Panel panel1;
+  }
+}
+
